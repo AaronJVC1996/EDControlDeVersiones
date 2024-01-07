15a0c889738049df09fb6b73a1da9427e892dd92
Aaron <avasquecar1@educacion.navarra.es>
2024-01-07 19:12:47 +0100

    He creado los atributos de la clase Jugador

--- src/Jugador.java
+++ src/Jugador.java
@@ -1,2 +1,11 @@
 public class Jugador {
+    private String Nombre;
+    private String Nacionalidad;
+    private int Edad;
+    private String Posicion;
+    private Boolean Lesionado;
+
+    public Jugador (){
+
+    }
 }
