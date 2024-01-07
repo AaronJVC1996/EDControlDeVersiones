fb3c11bbba95182b2bb1172eebd0e604392eaa8d
Aaron <avasquecar1@educacion.navarra.es>
2024-01-07 19:22:43 +0100

    He creado el constructor y tambien he creado todos los getters de esta clase.

--- src/Jugador.java
+++ src/Jugador.java
@@ -5,7 +5,26 @@ public class Jugador {
     private String Posicion;
     private Boolean Lesionado;
 
-    public Jugador (){
-
+    public Jugador (String nombr, String Nac, int eda, String posicio){
+        Nombre = nombr;
+        Nacionalidad = Nac;
+        Edad = eda;
+        Posicion = posicio;
+        Lesionado = false;
+    }
+    public String getNombre(){
+        return Nombre;
+    }
+    public String getNacionalidad(){
+        return Nacionalidad;
+    }
+    public int getEdad(){
+        return Edad;
+    }
+    public String getPosicion(){
+        return Posicion;
+    }
+    public Boolean getLesionado(){
+        return Lesionado;
     }
 }
