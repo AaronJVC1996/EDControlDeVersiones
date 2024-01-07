ed234d1cc529be9045912f318b519fb9404f75f4
Aaron <avasquecar1@educacion.navarra.es>
2024-01-07 19:34:14 +0100

    He arreglado el atributo numMaxJugadores añadiendole final y cree el constructor.

--- src/Equipo.java
+++ src/Equipo.java
@@ -1,6 +1,11 @@
 public class Equipo {
     private String Nombre;
     private String Ciudad;
-    private int numMaxJugadores;
+    private final int numMaxJugadores = 22;
     private Jugador[] ListaJugadores;
+
+    public Equipo (String nombr, String ciuda){
+        Nombre = nombr;
+        Ciudad = ciuda;
+    }
 }
