183f512c0f1f64c7c10c337f732673e7e551583e
Aaron <avasquecar1@educacion.navarra.es>
2024-01-07 21:50:13 +0100

    He creado el metodo mostrarlistadoequipos el getnumequipos el getequipo el getpais y añadirequipo, fueron complicados un poco, pero reutilize tambien codigo de la clase jugador.

--- src/Liga.java
+++ src/Liga.java
@@ -3,8 +3,42 @@ public class Liga {
     private final int numMaxEquipos = 20;
     private Equipo[] ListadoEquipos;
 
-    public Liga (String pai){
+    public Liga(String pai) {
         Pais = pai;
         ListadoEquipos = new Equipo[numMaxEquipos];
+    }
+
+    public void mostrarListadoEquipos() {
+        for (int i = 0; i < ListadoEquipos.length; i++) {
+            System.out.println(ListadoEquipos[i]);
+        }
+    }
+    public int getNumEquipos(){
+        int contador = 0;
+        for (int i = 0; i < ListadoEquipos.length; i++) {
+            if (ListadoEquipos[i] != null) {
+                contador++;
+            }
+        }
+        return contador;
+    }
+    public Equipo getEquipo(String nombreEquipo){
+        for (int i = 0; i < ListadoEquipos.length; i++) {
+            if (ListadoEquipos[i] != null && ListadoEquipos[i].getNombre().equals(nombreEquipo)) {
+                return ListadoEquipos[i];
+            }
+        }
+        return null;
+    }
+    public String getPais(){
+        return Pais;
+    }
+    public void añadirEquipo(Equipo equipo){
+        for (int i = 0; i < ListadoEquipos.length; i++) {
+            if (ListadoEquipos[i] == null) {
+                ListadoEquipos[i] = equipo;
+                break;
+            }
+        }
     }
 }
