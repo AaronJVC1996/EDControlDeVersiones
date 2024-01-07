d1916f39175e13acbaf6fc4089366c3edbbe4c68
Aaron <avasquecar1@educacion.navarra.es>
2024-01-07 21:26:07 +0100

    He creado los atributos y el constructor de liga, facil y sencillo.

--- src/Liga.java
+++ src/Liga.java
@@ -1,2 +1,10 @@
 public class Liga {
+    private String Pais;
+    private final int numMaxEquipos = 20;
+    private Equipo[] ListadoEquipos;
+
+    public Liga (String pai){
+        Pais = pai;
+        ListadoEquipos = new Equipo[numMaxEquipos];
+    }
 }
