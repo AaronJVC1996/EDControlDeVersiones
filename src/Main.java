bfd673fefd00375a81af6353849b9bfb0ae1b879
Aaron <avasquecar1@educacion.navarra.es>
2024-01-07 22:08:02 +0100

    He creado el inicio del Main, donde se escribe el pais, tambien he hecho que devuelva los datos puestos como datos.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Bienvenidos!");
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Empiece creando una liga:");
+        System.out.println("Escriba el país de la liga:");
+        String paisLiga = scanner.nextLine();
+        Liga liga = new Liga(paisLiga);
+        System.out.println("Liga en " + liga.getPais());
+        System.out.println("Número de equipos: " + liga.getNumEquipos());
     }
 }
