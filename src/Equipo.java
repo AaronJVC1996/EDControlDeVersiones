09d99283e1a82ceb277168d0bc3298f7d08eada9
Aaron <avasquecar1@educacion.navarra.es>
2024-01-07 21:19:56 +0100

    He creado los getters que era lo sensillo, tambien he creado los voids, lo cual me tarde un poco mas y busque en google ciertas cosas como el equals y tambien me ayude de internet para buscar como paso los nulls ala derecha.

--- src/Equipo.java
+++ src/Equipo.java
@@ -9,5 +9,43 @@ public class Equipo {
         Ciudad = ciuda;
         ListaJugadores = new Jugador[numMaxJugadores];
     }
+    public String getNombre (){
+        return Nombre;
+    }
+    public String getCiudad(){
+        return Ciudad;
+    }
+    public int getNumJugadores(){
+        int contador = 0;
+        for (int i = 0; i < ListaJugadores.length; i++) {
+            if (ListaJugadores[i] != null) {
+                contador++;
+            }
+        }
+        return contador;
+    }
+    public void adquirirJugador (Jugador jugador){
+        for (int i = 0; i < ListaJugadores.length; i++) {
+            if (ListaJugadores[i] == null) {
+                ListaJugadores[i] = jugador;
+                break;
+            }
+        }
+    }
+    public void venderJugador(String nombreJugador) {
+        for (int i = 0; i < ListaJugadores.length; i++) {
+            if (ListaJugadores[i] != null && ListaJugadores[i].getNombre().equals(nombreJugador)) {
 
+                ListaJugadores[i] = null;
+
+                for (int u = i; u < ListaJugadores.length - 1; u++) {
+                    ListaJugadores[u] = ListaJugadores[u + 1];
+                }
+
+                ListaJugadores[ListaJugadores.length - 1] = null;
+
+                break;
+            }
+        }
+    }
 }
